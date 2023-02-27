e05950fdd67faeee8d2e1726e3dc66fa599f38b1
pavan <rayabagipavan@gmail.com>
2023-02-27 14:58:44 +0530

    "error case added"

--- src/test/java/com/spy/app/spyTest.java
+++ src/test/java/com/spy/app/spyTest.java
@@ -8,10 +8,13 @@ import org.junit.jupiter.api.Test;
 
 class spyTest {
 
+	/**
+	 * 
+	 */
 	@Test
 	void test() {
 		assertTrue(spy.spyy(123));
-		assertFalse(spy.spyy(1234));
+		assertFalse(spy.spyy(123));
 	}
 
 }
